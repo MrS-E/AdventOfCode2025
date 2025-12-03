d0c30b0f63a468850ebce1d3afb1910631407c79
Simeon Stix <stixsim1@students.zhaw.ch>
2025-12-03 23:23:07 +0100

    day 3 part 2

--- day3.c
+++ day3.c
@@ -5,38 +5,59 @@
 #include <stdint.h>
 #include <string.h>
 
+#define POW10_TABLE \
+((const uint64_t[]){ \
+1ULL,                     /* 10^0  */ \
+10ULL,                    /* 10^1  */ \
+100ULL,                   /* 10^2  */ \
+1000ULL,                  /* 10^3  */ \
+10000ULL,                 /* 10^4  */ \
+100000ULL,                /* 10^5  */ \
+1000000ULL,               /* 10^6  */ \
+10000000ULL,              /* 10^7  */ \
+100000000ULL,             /* 10^8  */ \
+1000000000ULL,            /* 10^9  */ \
+10000000000ULL,           /* 10^10 */ \
+100000000000ULL,          /* 10^11 */ \
+1000000000000ULL          /* 10^12 */ \
+})
+
+#define POW10(n) (POW10_TABLE[(n)])
+
 
 static inline uint8_t getNum(char ch) {
     unsigned d = (unsigned)(ch - '0');
     return d < 10u ? (uint8_t)d : 0xFF;
 }
-static inline int getLargestCombination(const char *input, size_t len, uint8_t *num) {
+static inline int getLargestCombination(const char *input, size_t len, size_t numOfPtr, uint64_t *num) {
     if (len < 2) return -1;
-    const char *lptr = input;
-    const char *rptr = input + 1;
-    const char *p = input + 1;
+    const char *ptr;
     const char *end = input + len;
-    for (; p < end; ++p) {
-        if (*p > *lptr && p + 1 < end) {
-            lptr = p;
-            rptr = p + 1;
-        } else if (p > lptr && *p > *rptr) {
-            rptr = p;
+    const char *start = input;
+
+    for (size_t i = 0; i < numOfPtr; i++) {
+        const size_t remaining = numOfPtr - i;
+        const char *p = start;
+        ptr = p;
+
+        for (; p < (end - remaining + 1); ++p) {
+            if (*ptr < *p) {
+                ptr = p;
+            }
         }
+
+        start = ptr + 1;
+        *num += getNum(*ptr) * POW10(remaining-1);
     }
-    uint8_t L = getNum(*lptr);
-    uint8_t R = getNum(*rptr);
-    if (L == 0xFF || R == 0xFF) return -2;
-    *num = (uint8_t)(L * 10u + R);
+
     return 0;
 }
 
-int aocday3(char **input, size_t len, uint32_t *sum) {
+int aocday3(char **input, size_t len, uint64_t *sum) {
     for (size_t i = 0; i < len; i++) {
-        uint8_t num = 0;
-        if (getLargestCombination(input[i], strlen(input[i]), &num)) {
+        if (getLargestCombination(input[i], strlen(input[i]), 12, sum)) {
             return -1;
         }
-        *sum += num;
     }
+    return 0;
 }
