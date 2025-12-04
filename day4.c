2747bbaec3859ebd2e6f17596c13708493dcc4f1
Simeon Stix <simeon.stix@protonmail.ch>
2025-12-04 19:28:51 +0100

    day 4 part 2

--- day4.c
+++ day4.c
@@ -4,21 +4,46 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 void aocday4(char **f, size_t rows, size_t cols, uint16_t *sum) {
     const int dr[8] = { -1, -1, -1,  0, 0, 1, 1, 1 };
     const int dc[8] = { -1,  0,  1, -1, 1,-1, 0, 1 };
-    for (int r = 0; r < rows; r++) {
-        for (int c = 0; c < cols; c++) {
-            if (f[r][c] != '@') continue;
-            int n = 0;
-            for (int k = 0; k < 8; k++) {
-                int rr = r + dr[k];
-                int cc = c + dc[k];
-                if (rr < 0 || rr >= rows || cc < 0 || cc >= cols) continue;
-                if (f[rr][cc] == '@') n++;
-            }
-            if (n < 4) (*sum)++;
+    bool *rm = malloc(rows * cols * sizeof(bool));
+
+    for (;;) {
+        for (size_t i = 0; i < rows * cols; i++) {
+            rm[i] = false;
         }
+
+        uint32_t rrow = 0;
+
+        for (size_t r = 0; r < rows; r++) {
+            for (size_t c = 0; c < cols; c++) {
+                if (f[r][c] != '@') continue;
+                int n = 0;
+                for (int k = 0; k < 8; k++) {
+                    int rr = (int)r + dr[k];
+                    int cc = (int)c + dc[k];
+                    if (rr < 0 || rr >= (int)rows || cc < 0 || cc >= (int)cols) continue;
+                    if (f[rr][cc] == '@') n++;
+                }
+                if (n < 4) rm[r * cols + c] = true;
+            }
+        }
+
+        for (size_t r = 0; r < rows; r++) {
+            for (size_t c = 0; c < cols; c++) {
+                if (rm[r * cols + c]) {
+                    f[r][c] = '.';
+                    rrow++;
+                }
+            }
+        }
+
+        if (rrow == 0) break;
+        *sum += rrow;
     }
+    free(rm);
 }
