12b9c65631d42ea14580846bdf46aa2d9c699480
Simeon Stix <simeon.stix@protonmail.ch>
2025-12-11 23:20:42 +0100

    day 11 part 2

--- day11.c
+++ day11.c
@@ -10,8 +10,10 @@
 #define NAME_MAX_LEN 64
 #define PATHS_OK 0
 #define PATHS_ERR_INVALID_INPUT 1
-#define PATHS_ERR_NO_YOU 2
+#define PATHS_ERR_NO_SVR 2
 #define PATHS_ERR_NO_OUT 3
+#define PATHS_ERR_NO_DAC 4
+#define PATHS_ERR_NO_FFT 5
 #define PATHS_ERR_CYCLE 4
 #define PATHS_ERR_OOM 5
 #define PATHS_ERR_OVERFLOW 6
@@ -26,33 +28,46 @@ typedef struct {
    char name[NAME_MAX_LEN];
 } Node;
 
-static char *trim_ws(char *s) {
-   while (*s && isspace((unsigned char)*s))
-      s++;
-   char *end = s + strlen(s);
-   while (end > s && isspace((unsigned char)end[-1]))
-      end--;
-   *end = '\0';
-   return s;
-}
-
 static void strip_crlf(char *s) {
-   for (; *s; s++) {
+   for (; *s; s++)
       if (*s == '\r' || *s == '\n') {
          *s = '\0';
          return;
       }
+}
+
+static char *trim_ws(char *s) {
+   while (*s && isspace((unsigned char)*s))
+      s++;
+   char *e = s + strlen(s);
+   while (e > s && isspace((unsigned char)e[-1]))
+      e--;
+   *e = '\0';
+   return s;
+}
+
+static void normalize_name_inplace(char *s) {
+   unsigned char *u = (unsigned char *)s;
+   if (u[0] == 0xEF && u[1] == 0xBB && u[2] == 0xBF) {
+      memmove(s, s + 3, strlen(s + 3) + 1);
    }
+   char *w = s;
+   for (char *r = s; *r; r++) {
+      unsigned char c = (unsigned char)*r;
+      if (isalnum(c) || c == '_')
+         *w++ = (char)c;
+   }
+   *w = '\0';
 }
 
 static int add_edge(Adj *adj, int from, int to) {
    if (adj[from].cnt == adj[from].cap) {
-      size_t new_cap = adj[from].cap ? adj[from].cap * 2 : 4;
-      int *tmp = (int *)realloc(adj[from].to, new_cap * sizeof(int));
+      size_t nc = adj[from].cap ? adj[from].cap * 2 : 4;
+      int *tmp = realloc(adj[from].to, nc * sizeof(int));
       if (!tmp)
          return 0;
       adj[from].to = tmp;
-      adj[from].cap = new_cap;
+      adj[from].cap = nc;
    }
    adj[from].to[adj[from].cnt++] = to;
    return 1;
@@ -60,24 +75,23 @@ static int add_edge(Adj *adj, int from, int to) {
 
 static int find_or_add(Node **nodes, Adj **adj, size_t *count, size_t *cap,
                        const char *name) {
-   for (size_t i = 0; i < *count; i++) {
+   for (size_t i = 0; i < *count; i++)
       if (strcmp((*nodes)[i].name, name) == 0)
          return (int)i;
-   }
 
    if (*count == *cap) {
-      size_t new_cap = *cap ? *cap * 2 : 64;
-      Node *n_tmp = realloc(*nodes, new_cap * sizeof(Node));
-      Adj *a_tmp = realloc(*adj, new_cap * sizeof(Adj));
+      size_t nc = *cap ? *cap * 2 : 64;
+      Node *n_tmp = realloc(*nodes, nc * sizeof(Node));
+      Adj *a_tmp = realloc(*adj, nc * sizeof(Adj));
       if (!n_tmp || !a_tmp)
          return -1;
       *nodes = n_tmp;
       *adj = a_tmp;
-      for (size_t i = *cap; i < new_cap; i++) {
+      for (size_t i = *cap; i < nc; i++) {
          (*adj)[i].to = NULL;
          (*adj)[i].cnt = (*adj)[i].cap = 0;
       }
-      *cap = new_cap;
+      *cap = nc;
    }
 
    strncpy((*nodes)[*count].name, name, NAME_MAX_LEN - 1);
@@ -85,34 +99,43 @@ static int find_or_add(Node **nodes, Adj **adj, size_t *count, size_t *cap,
    return (int)(*count)++;
 }
 
-static int dfs_count(int u, int out_id, Adj *adj, uint64_t *memo,
-                     uint8_t *visiting, uint64_t *result) {
-   if (u == out_id) {
-      *result = 1;
-      return PATHS_OK;
-   }
-   if (memo[u] != UINT64_MAX) {
-      *result = memo[u];
-      return PATHS_OK;
-   }
-   if (visiting[u])
-      return PATHS_ERR_CYCLE;
+static int dfs_count_state(int u, int out_id, int dac_id, int fft_id, Adj *adj,
+                           uint64_t *memo, uint8_t *visiting, uint8_t mask,
+                           uint64_t *result) {
+   if (u == dac_id)
+      mask |= 1;
+   if (u == fft_id)
+      mask |= 2;
 
-   visiting[u] = 1;
+   if (u == out_id) {
+      *result = (mask == 3) ? 1u : 0u;
+      return PATHS_OK;
+   }
+
+   size_t key = (size_t)u * 4u + (size_t)mask;
+   if (memo[key] != UINT64_MAX) {
+      *result = memo[key];
+      return PATHS_OK;
+   }
+
+   if (visiting[key])
+      return PATHS_ERR_CYCLE;
+   visiting[key] = 1;
+
    uint64_t sum = 0;
    for (size_t i = 0; i < adj[u].cnt; i++) {
       uint64_t sub = 0;
-      int err = dfs_count(adj[u].to[i], out_id, adj, memo, visiting, &sub);
+      int err = dfs_count_state(adj[u].to[i], out_id, dac_id, fft_id, adj, memo,
+                                visiting, mask, &sub);
       if (err)
          return err;
-
       if (UINT64_MAX - sum < sub)
          return PATHS_ERR_OVERFLOW;
       sum += sub;
    }
 
-   visiting[u] = 0;
-   memo[u] = sum;
+   visiting[key] = 0;
+   memo[key] = sum;
    *result = sum;
    return PATHS_OK;
 }
@@ -127,25 +150,16 @@ int aocday11(char **lines, size_t n_lines, uint64_t *out) {
    size_t node_count = 0, node_cap = 0;
 
    for (size_t i = 0; i < n_lines; i++) {
-      if (!lines[i]) {
-         // ignore null line pointers
+      if (!lines[i])
          continue;
-      }
 
       char *dup = strdup(lines[i]);
-      if (!dup) {
-         // OOM
-         for (size_t k = 0; k < node_count; k++)
-            free(adj[k].to);
-         free(nodes);
-         free(adj);
-         return PATHS_ERR_OOM;
-      }
-
+      if (!dup)
+         goto oom;
       strip_crlf(dup);
+
       char *whole = trim_ws(dup);
       if (*whole == '\0') {
-         // skip empty lines
          free(dup);
          continue;
       }
@@ -155,10 +169,10 @@ int aocday11(char **lines, size_t n_lines, uint64_t *out) {
          free(dup);
          goto invalid;
       }
-
       *colon = '\0';
+
       char *lhs = trim_ws(whole);
-      char *rhs = trim_ws(colon + 1);
+      normalize_name_inplace(lhs);
       if (*lhs == '\0') {
          free(dup);
          goto invalid;
@@ -170,89 +184,119 @@ int aocday11(char **lines, size_t n_lines, uint64_t *out) {
          goto oom;
       }
 
-      if (*rhs) {
-         char *save = NULL;
-         for (char *tok = strtok_r(rhs, " \t", &save); tok;
-              tok = strtok_r(NULL, " \t", &save)) {
+      char *rhs = trim_ws(colon + 1);
+      char *save = NULL;
+      for (char *tok = strtok_r(rhs, " \t,", &save); tok;
+           tok = strtok_r(NULL, " \t,", &save)) {
+         tok = trim_ws(tok);
+         normalize_name_inplace(tok);
+         if (*tok == '\0')
+            continue;
 
-            tok = trim_ws(tok);
-            if (*tok == '\0')
-               continue;
-
-            int to = find_or_add(&nodes, &adj, &node_count, &node_cap, tok);
-            if (to < 0) {
-               free(dup);
-               goto oom;
-            }
-            if (!add_edge(adj, from, to)) {
-               free(dup);
-               goto oom;
-            }
+         int to = find_or_add(&nodes, &adj, &node_count, &node_cap, tok);
+         if (to < 0) {
+            free(dup);
+            goto oom;
+         }
+         if (!add_edge(adj, from, to)) {
+            free(dup);
+            goto oom;
          }
       }
-      free(dup);
-      continue;
 
-   invalid:
       free(dup);
-      for (size_t k = 0; k < node_count; k++)
-         free(adj[k].to);
-      free(nodes);
-      free(adj);
-      return PATHS_ERR_INVALID_INPUT;
-
-   oom:
-      free(dup);
-      for (size_t k = 0; k < node_count; k++)
-         free(adj[k].to);
-      free(nodes);
-      free(adj);
-      return PATHS_ERR_OOM;
    }
 
-   int you_id = -1, out_id = -1;
+   int svr_id = -1, out_id = -1, dac_id = -1, fft_id = -1;
    for (size_t i = 0; i < node_count; i++) {
-      if (strcmp(nodes[i].name, "you") == 0)
-         you_id = (int)i;
+      if (strcmp(nodes[i].name, "svr") == 0)
+         svr_id = (int)i;
       if (strcmp(nodes[i].name, "out") == 0)
          out_id = (int)i;
-   }
-   if (you_id < 0) {
-      for (size_t k = 0; k < node_count; k++)
-         free(adj[k].to);
-      free(nodes);
-      free(adj);
-      return PATHS_ERR_NO_YOU;
-   }
-   if (out_id < 0) {
-      for (size_t k = 0; k < node_count; k++)
-         free(adj[k].to);
-      free(nodes);
-      free(adj);
-      return PATHS_ERR_NO_OUT;
+      if (strcmp(nodes[i].name, "dac") == 0)
+         dac_id = (int)i;
+      if (strcmp(nodes[i].name, "fft") == 0)
+         fft_id = (int)i;
    }
 
-   uint64_t *memo = malloc(node_count * sizeof(uint64_t));
-   uint8_t *visiting = calloc(node_count, 1);
+   if (svr_id < 0) {
+      goto no_svr;
+   }
+   if (out_id < 0) {
+      goto no_out;
+   }
+   if (dac_id < 0) {
+      goto no_dac;
+   }
+   if (fft_id < 0) {
+      goto no_fft;
+   }
+
+   // memo size: node_count * 4
+   uint64_t *memo = malloc(node_count * 4u * sizeof(uint64_t));
+   uint8_t *visiting = calloc(node_count * 4u, 1);
    if (!memo || !visiting) {
       free(memo);
       free(visiting);
-      for (size_t k = 0; k < node_count; k++)
-         free(adj[k].to);
-      free(nodes);
-      free(adj);
-      return PATHS_ERR_OOM;
+      goto oom;
    }
-   for (size_t i = 0; i < node_count; i++)
+
+   for (size_t i = 0; i < node_count * 4u; i++)
       memo[i] = UINT64_MAX;
 
-   int err = dfs_count(you_id, out_id, adj, memo, visiting, out);
+   uint64_t ans = 0;
+   int err = dfs_count_state(svr_id, out_id, dac_id, fft_id, adj, memo,
+                             visiting, 0, &ans);
+   if (err == PATHS_OK)
+      *out = ans;
 
    free(memo);
    free(visiting);
-   for (size_t k = 0; k < node_count; k++)
-      free(adj[k].to);
+   for (size_t i = 0; i < node_count; i++)
+      free(adj[i].to);
    free(nodes);
    free(adj);
    return err;
+
+invalid:
+   for (size_t i = 0; i < node_count; i++)
+      free(adj[i].to);
+   free(nodes);
+   free(adj);
+   return PATHS_ERR_INVALID_INPUT;
+
+oom:
+   for (size_t i = 0; i < node_count; i++)
+      free(adj[i].to);
+   free(nodes);
+   free(adj);
+   return PATHS_ERR_OOM;
+
+no_svr:
+   for (size_t i = 0; i < node_count; i++)
+      free(adj[i].to);
+   free(nodes);
+   free(adj);
+   return PATHS_ERR_NO_SVR;
+
+no_out:
+   for (size_t i = 0; i < node_count; i++)
+      free(adj[i].to);
+   free(nodes);
+   free(adj);
+   return PATHS_ERR_NO_OUT;
+
+no_dac:
+   for (size_t i = 0; i < node_count; i++)
+      free(adj[i].to);
+   free(nodes);
+   free(adj);
+   return PATHS_ERR_NO_DAC;
+
+no_fft:
+   for (size_t i = 0; i < node_count; i++)
+      free(adj[i].to);
+   free(nodes);
+   free(adj);
+   return PATHS_ERR_NO_FFT;
 }
